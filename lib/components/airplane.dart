4137320883d9c0281809e3872f50f35f3fb15444
tonynowater <tonynowater87@gmai..com>
2024-08-24 22:22:52 +0800

    add joystick to move the airplane

---
+++ lib/components/airplane.dart
@@ -0,0 +1,28 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+
+class Airplane extends SpriteAnimationComponent
+    with HasGameRef, CollisionCallbacks {
+  Airplane() : super(size: Vector2(50, 75), anchor: Anchor.center);
+
+  @override
+  Future<void> onLoad() async {
+    position = game.size / 2;
+    animation = await game.loadSpriteAnimation(
+      'airplane.png',
+      SpriteAnimationData.sequenced(
+        stepTime: 0.2,
+        amount: 4,
+        textureSize: Vector2(32, 39),
+      ),
+    );
+  }
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    super.onCollisionStart(intersectionPoints, other);
+  }
+}
--- lib/training_999.dart
+++ lib/training_999.dart
@@ -1,8 +1,79 @@
-import 'dart:ui';
-
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:training999/components/airplane.dart';
 
-class Training999 extends FlameGame {
+class Training999 extends FlameGame
+    with DragCallbacks, HasKeyboardHandlerComponents {
+  late Airplane _player;
+  late JoystickComponent joystick;
+
   @override
   Color backgroundColor() => const Color(0xFF211F30);
+
+  @override
+  Future onLoad() async {
+    await images.loadAllImages();
+    add(_player = Airplane());
+    addJoystick();
+  }
+
+  @override
+  void update(double dt) {
+    updateJoystick();
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      priority: 10,
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+        _player.position += Vector2(-1, 0);
+        break;
+      case JoystickDirection.upLeft:
+        _player.position += Vector2(-0.75, -0.75);
+        break;
+      case JoystickDirection.up:
+        _player.position += Vector2(0, -1);
+        break;
+      case JoystickDirection.upRight:
+        _player.position += Vector2(0.75, -0.75);
+        break;
+      case JoystickDirection.right:
+        _player.position += Vector2(1, 0);
+        break;
+      case JoystickDirection.downRight:
+        _player.position += Vector2(0.75, 0.75);
+        break;
+      case JoystickDirection.down:
+        _player.position += Vector2(0, 1);
+        break;
+      case JoystickDirection.downLeft:
+        _player.position += Vector2(-0.75, 0.75);
+        break;
+      default:
+        _player.position += Vector2(0, 0);
+        break;
+    }
+    _player.position += joystick.relativeDelta;
+  }
 }
