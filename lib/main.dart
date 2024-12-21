ff4265a0dbbe5c63b1d906e52bc1afbd110d39a0
tonynowater <tonynowater87@gmai..com>
2024-12-21 21:56:47 +0800

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flame/flame.dart';
 import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:training999/firebase_options.dart';
 import 'package:training999/training_999.dart';
 import 'package:training999/util/my_provider_observer.dart';
 import 'package:training999/widget/ranking_list_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
 
